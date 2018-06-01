70cb3eb8b5e9d62ff3257c93987d4ce4f8aa1a8e
elias-po <thendbims@gmail.com>
2018-06-02 02:08:19 +0300

    added an option to hide stars

--- app/src/main/java/com/example/elias/popularmovies_stage1/adapter/PosterViewHolder.java
+++ app/src/main/java/com/example/elias/popularmovies_stage1/adapter/PosterViewHolder.java
@@ -19,13 +19,19 @@ import static com.example.elias.popularmovies_stage1.Utils.isFavourite;
 public class PosterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private ImageView poster;
     private ImageView star_btn;
+    private boolean HIDE_STARS = true; // whether to show stars on the posters activity
     private Movie movie;
 
     public PosterViewHolder(final View itemView) {
         super(itemView);
         poster = (ImageView) itemView.findViewById(R.id.poster_image);
         star_btn = (ImageView) itemView.findViewById(R.id.star_btn);
-        itemView.setOnClickListener(this);
+        if(HIDE_STARS) {
+            itemView.setOnClickListener(this);
+        } else {
+            poster.setOnClickListener(this);
+            star_btn.setOnClickListener(this);
+        }
     }
 
 
@@ -34,29 +40,37 @@ public class PosterViewHolder extends RecyclerView.ViewHolder implements View.On
                 movie.getPosterPath();
         this.movie = movie;
         Picasso.with(itemView.getContext()).load(poster_url).fit().into(poster);
-        if(isFavourite())
-            star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_on));
-        else
-            star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_off));
+        if(HIDE_STARS){
+            star_btn.setVisibility(View.INVISIBLE);
+        } else {
+            if (isFavourite())
+                star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_on));
+            else
+                star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_off));
+        }
     }
 
     @Override
     public void onClick(View v) {
-        Log.i("TAG", "You clicked cell at position " + getAdapterPosition());
-        Intent intent = new Intent(v.getContext(), MovieDetailActivity.class);
+        if(v.getId() == poster.getId() || HIDE_STARS) {
+            Log.i("TAG", "You clicked cell at position " + getAdapterPosition());
+            Intent intent = new Intent(v.getContext(), MovieDetailActivity.class);
 
-        Bundle extras = new Bundle();
-        extras.putString(MovieDetailActivity.movie_id, movie.getId().toString());
-        extras.putString(MovieDetailActivity.movie_title, movie.getTitle());
-        extras.putString(MovieDetailActivity.movie_release_date, movie.getReleaseDate());
-        extras.putString(MovieDetailActivity.movie_user_rating, movie.getVoteAverage().toString());
-        extras.putString(MovieDetailActivity.movie_overview, movie.getOverview());
-        String poster_url = v.getResources().getString(R.string.image_base_url)
-                + v.getResources().getString(R.string.image_size_default)
-                + movie.getPosterPath();
-        extras.putString(MovieDetailActivity.movie_poster_url, poster_url);
-        intent.putExtras(extras);
+            Bundle extras = new Bundle();
+            extras.putString(MovieDetailActivity.movie_id, movie.getId().toString());
+            extras.putString(MovieDetailActivity.movie_title, movie.getTitle());
+            extras.putString(MovieDetailActivity.movie_release_date, movie.getReleaseDate());
+            extras.putString(MovieDetailActivity.movie_user_rating, movie.getVoteAverage().toString());
+            extras.putString(MovieDetailActivity.movie_overview, movie.getOverview());
+            String poster_url = v.getResources().getString(R.string.image_base_url)
+                    + v.getResources().getString(R.string.image_size_default)
+                    + movie.getPosterPath();
+            extras.putString(MovieDetailActivity.movie_poster_url, poster_url);
+            intent.putExtras(extras);
 
-        v.getContext().startActivity(intent);
+            v.getContext().startActivity(intent);
+        } else {
+            Log.i("TAG", "You clicked STAR at position " + getAdapterPosition());
+        }
     }
 }
