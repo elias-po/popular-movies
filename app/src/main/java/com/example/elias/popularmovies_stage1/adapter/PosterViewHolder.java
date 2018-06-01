6165fd0f174dcb67c8978a463318516d446b6fae
elias-po <thendbims@gmail.com>
2018-06-02 01:19:46 +0300

    accomodations for favourites feature

--- app/src/main/java/com/example/elias/popularmovies_stage1/adapter/PosterViewHolder.java
+++ app/src/main/java/com/example/elias/popularmovies_stage1/adapter/PosterViewHolder.java
@@ -13,14 +13,18 @@ import com.example.elias.popularmovies_stage1.R;
 import com.example.elias.popularmovies_stage1.model.Movie;
 import com.squareup.picasso.Picasso;
 
+import static com.example.elias.popularmovies_stage1.Utils.isFavourite;
+
 
 public class PosterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private ImageView poster;
+    private ImageView star_btn;
     private Movie movie;
 
     public PosterViewHolder(final View itemView) {
         super(itemView);
         poster = (ImageView) itemView.findViewById(R.id.poster_image);
+        star_btn = (ImageView) itemView.findViewById(R.id.star_btn);
         itemView.setOnClickListener(this);
     }
 
@@ -30,6 +34,10 @@ public class PosterViewHolder extends RecyclerView.ViewHolder implements View.On
                 movie.getPosterPath();
         this.movie = movie;
         Picasso.with(itemView.getContext()).load(poster_url).fit().into(poster);
+        if(isFavourite())
+            star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_on));
+        else
+            star_btn.setImageDrawable(context.getResources().getDrawable(android.R.drawable.btn_star_big_off));
     }
 
     @Override
