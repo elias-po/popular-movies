cfd309bf930508b21ba24adafea2d6efea41e472
elias-po <thendbims@gmail.com>
2018-06-12 17:47:12 +0300

    approach adjustment

--- app/src/main/java/com/example/elias/popular_movies/data/FavouriteDbHelper.java
+++ app/src/main/java/com/example/elias/popular_movies/data/FavouriteDbHelper.java
@@ -11,27 +11,30 @@ public class FavouriteDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "fav_movies.db";
     private static final int VERSION = 1;
 
+    private static final String CREATE_TABLE = "CREATE TABLE " + FavMovieEntry.TABLE_NAME + " (" +
+            FavMovieEntry._ID + " INTEGER PRIMARY KEY, " +
+            FavMovieEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
+            FavMovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+            FavMovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+            FavMovieEntry.COLUMN_VOTE_AVERAGE + " TEXT NOT NULL, " +
+            FavMovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
+            FavMovieEntry.COLUMN_POSTER_URL + " TEXT NOT NULL);";
+
+    private static final String ALTER_TABLE = "ALTER TABLE " + FavMovieEntry.TABLE_NAME;
+
     FavouriteDbHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String CREATE_TABLE = "CREATE TABLE " + FavMovieEntry.TABLE_NAME + " (" +
-                FavMovieEntry._ID + " INTEGER PRIMARY KEY, " +
-                FavMovieEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
-                FavMovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
-                FavMovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
-                FavMovieEntry.COLUMN_VOTE_AVERAGE + " TEXT NOT NULL, " +
-                FavMovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL, " +
-                FavMovieEntry.COLUMN_POSTER_URL + " TEXT NOT NULL);";
 
         db.execSQL(CREATE_TABLE);
     }
 
+    // approach inspired by https://thebhwgroup.com/blog/how-android-sqlite-onupgrade
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + FavMovieEntry.TABLE_NAME);
-        onCreate(db);
+        db.execSQL(ALTER_TABLE);
     }
 }
